b5a11a39f5a239879a98d7fdabf0c4a9a1401de9
Psaruklily <liliia.psaruk@pickethomes.com>
2021-08-31 16:43:41 +0300

    Added query params to query string

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,13 +4,15 @@ import {ButtonFilter} from '../ButtonFilter';
 import HousesService from '../../services/Houses-service';
 import './style.css';
 import Loader from '../Loader/LoaderComponent';
-import {withRouter} from "react-router-dom";
+import {withRouter, useHistory} from "react-router-dom";
 
-const Home = ({textFromInput}) => {
+const Home = ({textFromInput}, props) => {
 
     const [allHouses, setAllHouses] = useState([]);
     const [filteredHouses, setFilteredHouses] = useState(allHouses);
     const [loader, setLoader] = useState(false);
+    // const [query, setQuery] = useState(1);
+    const history = useHistory()
     const service = new HousesService();
     
     useEffect(() => {
@@ -31,7 +33,15 @@ const Home = ({textFromInput}) => {
             return house.address.toLowerCase().search(textFromInput) !== -1;
         });
         setFilteredHouses(result);
-    }, [textFromInput])
+    }, [textFromInput]);
+    
+
+    const saveBeds = () => {
+        const query = new URLSearchParams();
+        const value = document.getElementById('beds').value;
+        query.set("beds", value);
+        history.push(`/houses/?${query}`);
+    }
 
     return (
         <div>
@@ -45,6 +55,23 @@ const Home = ({textFromInput}) => {
                 </div>
    
                 <div className='page-wrapper'>
+
+
+                    <div className='filter-dropdown-menu'>
+                        <div className='filter-dropdown-menu_filters'>
+                            <label for='beds'>Beds:</label>
+                            <select name='beds' id='beds'>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                            </select><br/><br/>
+                            <input type="submit" value="Save" onClick={saveBeds}></input>
+                        </div>
+                        <div className='filter-dropdown-menu_buttons'></div>
+                    </div>
+
+
                     {filteredHouses.length === 0 && <p className='not-available'>0 home available</p>}
                     {loader && <Loader />}
                     <Houses houses={filteredHouses}/>
